553e9ac205b886d42c8b9c940d4509e5de9499dc
izr8809 <izr8809@gmail.com>
2016-05-13 14:57:15 +0900

    기본세팅

--- sharecloset/settings.py
+++ sharecloset/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party
+    'debug_toolbar',
+    'bootstrap3',
+    #local
     'product',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -129,4 +134,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 
