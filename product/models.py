6e1f141e7c801369ff700b9cdb2672c4cead8131
izr8809 <izr8809@gmail.com>
2016-05-19 20:40:43 +0900

    사진추가

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Product(models.Model):
+    title = models.CharField(max_length=20)
+    main_photo = models.ImageField()
+    detail_photo = models.ImageField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
 # Create your models here.
+
--- product/views.py
+++ product/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from .models import Product
 
 
 def intro(request):
     return render(request, 'product/intro.html')
+
+
 def product_list(request):
-    return render(request, 'product/detail.html')
+    product_list = Product.objects.all()
+    return render(request, 'product/detail.html', {
+    'product_list':product_list})
 
 # Create your views here.
